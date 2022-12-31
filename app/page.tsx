6a74db56f5d85b0f90eb2b01fa0a5137a8c12efa
DumeGrisoni <misanthrope20218@gmail.com>
2022-12-31 11:05:53 +0100

    Install Sanity Backend

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function HomePage() {
+  return (
+    <div>
+        <h1 className='text-4xl'>Bienvenue sur GaMinth Blog</h1>
+        <p>Le blog sur la création d'asset de jeux vidéo français</p>
+    </div>
+  )
+}
+
+export default HomePage
